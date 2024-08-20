f114690386f3855432bbfd2589e0a89731c2c1e2
Mohit kumar <mk513147@gmail.com>
2024-08-20 19:00:20 +0530

    Added Validation methods

--- Backend/Routes/auth.js
+++ Backend/Routes/auth.js
@@ -1,14 +1,24 @@
 const express = require('express')
 const User = require('../Models/User')
 const router = express.Router();
+const {body, validationResult} = require('express-validator');
 
 //Creating a user using: POST "/api/auth"
 
-router.post('/', (req, res)=>{
-console.log(req.body);
-const user = User(req.body);
-user.save();
-res.send(req.body);
+router.post('/', [
+    body('name', 'Enter more tha 2 Words').isLength({min: 3}),
+    body('email','Enter a valid email').isEmail(),
+    body('password', 'Password should be more than 5 letters').isLength({min: 6})
+], (req, res)=>{
+const errors = validationResult(req);
+if(!errors.isEmpty()){
+    return res.status(400).json({errors: errors.array()});
+}
+User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  }).then(user => res.json(user));
 })
 
 module.exports = router;
