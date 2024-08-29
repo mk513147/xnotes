d8713f2407e2d6e4c733aa4f99ab2524fc3c4057
Mohit kumar <mk513147@gmail.com>
2024-08-29 10:47:21 +0530

    used location hook in navbar.

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,16 +1,10 @@
-import React, { useContext, useEffect } from 'react';
-import NoteContext from '../Context/notes/NoteContext';
+import React from 'react';
 
 const About = ()=> {
-  const a = useContext(NoteContext);
 
-  useEffect(()=>{
-    a.update();
-    // eslint-disable-next-line
-  }, [])
   return (
     <div>
-      This is about {a.state.name} and he is in class {a.state.class}.
+      This is about.
     </div>
   )
 }
--- src/Context/notes/NoteState.js
+++ src/Context/notes/NoteState.js
@@ -1,24 +1,8 @@
-import { useState } from 'react';
 import NoteContext from './NoteContext';
 
 const NoteState = (props)=>{
-    const s1  = {
-        "name": "Mohit",
-        "class": "2A",
-    }
-
-    const [state, setState] = useState(s1);
-
-    const update = ()=>{
-        setTimeout(()=>{
-            setState({
-                "name": "Ada Pada",
-                "class": "3c"
-            })
-        }, 2000);
-    }
     return(
-    <NoteContext.Provider value = {{state, update}}>
+    <NoteContext.Provider value = {{}}>
         {props.children}
     </NoteContext.Provider>
     )
